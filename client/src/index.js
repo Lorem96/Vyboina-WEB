b4e62aff3942b3fc9477b6860a31a52cf182ae0b
Nikolay Datsko <sayber1990@gmail.com>
2019-06-05 14:23:47 +0300

    Redux saga added

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 import * as reducers from '../src/reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const reducer = combineReducers(reducers);
+const sagaMiddleware = createSagaMiddleware();
 const rootElement = document.getElementById('root');
-const middleware = [reduxThunk];
+const middleware = [reduxThunk, sagaMiddleware];
 const store = createStore(
     reducer,
     applyMiddleware(...middleware)
