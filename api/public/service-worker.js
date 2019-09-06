367556965e7502d1c57438198bd9c0953d4daab4
Nikolay Datsko <sayber1990@gmail.com>
2019-09-06 20:40:20 +0300

    build

--- api/public/service-worker.js
+++ api/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.7ae1774270acba305701d1260659dbf2.js"
 );
 
 self.addEventListener('message', (event) => {
